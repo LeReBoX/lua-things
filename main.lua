8f3a492d36e6bfbb17e8c85bde40a09fa7bb42c3
LeReBoX <73342682+LeReBoX@users.noreply.github.com>
2021-04-28 11:34:52 +0300

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,2 +1,17 @@
-local hey = "hello."
-print("hey")
+head = game.Players.LocalPlayer.Character.Head
+headmesh = head.Mesh
+torsoguy = game.Players.LocalPlayer.Character.Torso
+rightleg = game.Players.LocalPlayer.Character:FindFirstChild("Right Leg")
+leftleg = game.Players.LocalPlayer.Character:FindFirstChild("Left Leg")
+headmesh.MeshType = Enum.MeshType.Wedge
+torsoguy.Transparency = 0
+if rightleg == nil then
+print("reset lol")
+else
+    rightleg:Remove()
+end
+if leftleg == nil then
+print("reset lol")
+else
+leftleg:Remove()
+end
