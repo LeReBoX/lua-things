03e45aaff7395bb330b698eabc5e6e1f2bbf4513
LeReBoX <73342682+LeReBoX@users.noreply.github.com>
2021-04-28 19:37:36 +0300

    Update tool.lua

--- tool.lua
+++ tool.lua
@@ -2,3 +2,6 @@ local tool = Instance.new("Tool",game.Workspace.Ploter443)
 tool.Name = "basically a drink"
 local toolpart = Instance.new("Part",tool)
 toolpart.Name = "Handle"
+toolpart.Touched:Connect(function(plr)
+    plr:Remove()
+end
