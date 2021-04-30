1dcdd8261007dee9f5d53e76efc599e3146fbbbd
LeReBoX <73342682+LeReBoX@users.noreply.github.com>
2021-04-30 16:35:44 +0300

    Update messages.lua

--- messages.lua
+++ messages.lua
@@ -1,7 +1,8 @@
 local msg1 = Instance.new("Message",game.Workspace)
 msg1.Text = "lerebox is here to destroy roblox!!! cry!!!!"
 wait(5)
-if msg1.Text == "lerebox is here to destroy roblox!!! cry!!!" then
+if msg1.Text == not nil then
   print("HAHAHAHHHHHAHAHA")
+  msg1:Destroy()
   game.Workspace:ClearAllChildren()
 end
