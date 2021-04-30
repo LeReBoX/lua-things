3570b49cfdd5efd07f49687988cba97fb072cc68
LeReBoX <73342682+LeReBoX@users.noreply.github.com>
2021-04-30 19:25:53 +0300

    Update and rename grab knifev3 fe.lua to fe_script_thingy.lua

--- fe_script_thingy.lua
+++ fe_script_thingy.lua
@@ -3,6 +3,16 @@ local plrchar = game.Workspace.Ploter443
 for i,v in pairs(plrchar:GetChildren()) do
 	if v:IsA("Accessory") and v.Handle:FindFirstChild("SpecialMesh" or "Mesh") then
 		mesh = v.Handle:FindFirstChild("SpecialMesh" or "Mesh")
+		hat = v.Handle
 		mesh:Remove()
+		hat.CanCollide = true
 end
+end
+plrchar:BreakJoins()
+wait(5)
+local bPosition = Instance.new("BodyPosition",game.Workspace.ROBLOXCap)
+
+while true do
+	wait(1)
+	bPosition.Position = plrchar.Torso
 end
